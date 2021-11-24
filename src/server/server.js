7791f968552d69b983f21087b8aa10407572e2d5
Joanna YU <joannayu@Haos-iMac.local>
2021-11-24 15:07:08 +1030

    get weatherbit data

--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 const { get } = require('http');
 app.use(cors());
 const url = require('url');
+const { response } = require('express');
 
 app.use(express.static('dist'))
 
@@ -15,15 +16,10 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-// let weatherbitUrl = "https://api.weatherbit.io/v2.0/forecast/daily?";    
-// let weatherbitLocation = "&"
-// let weatherbitAPI = "&key=edcede910dab453eaac6a57b71c10fee";
-
 
 const getPostCode = async(postcode) => {
-    // let postCode = document.getElementById('postcode').value
     let geoLocation = {
-        postalcode: postcode,
+        postalcode: "5063",
         maxRows: '10',
         username: 'zyy314jh'
     }
@@ -33,20 +29,48 @@ const getPostCode = async(postcode) => {
 
     let res = await axios.get(geonames_url);
 
-    let data = res.data;
+    let geonameslng = res.data.postalCodes[0].lng;
+    let geonameslat = res.data.postalCodes[0].lat;
 
-    console.log(data);
-    
+    console.log(geonameslng);
+    console.log(geonameslat)
+  getWeather(geonameslng, geonameslat, updataUI)
 }
+getPostCode();
 
+// app.get('/getlocation', function(req,res) {
+//     let postCode = req.body;
+//     getPostCode(postCode);
 
-app.get('/getlocation', function(req,res) {
-    let postCode = req.body;
-    getPostCode(postCode);
+// })
 
-})
+const getWeather = async(lng, lat, callback) => {
 
+    let weatherbitUrl = "https://api.weatherbit.io/v2.0/forecast/daily?";    
+    let weatherbitLocation = `&lat=${lat}&lon=${lng}`;
+    let weatherbitAPI = "&key=edcede910dab453eaac6a57b71c10fee";
+    
+    let weatherbit_url = weatherbitUrl + weatherbitLocation + weatherbitAPI;
+    console.log(weatherbit_url)
 
+    axios.get(weatherbit_url)
+    .then((response) => {
+        let weatherResults ={
+           "temperature": response.data.data[0].temp,
+           "wind": response.data.data[0].wind_spd,
+           "cloud": response.data.data[0].clouds,
+
+        }
+        console.log(weatherResults)
+        callback(weatherResults)
+    }, (error) => {
+        console.log(error);
+    });
+
+}
+function updateUI () {
+
+}
 
 
 
@@ -66,7 +90,7 @@ app.get('/getlocation', function(req,res) {
 
 let PixabayUrl = "https://pixabay.com/api/?key=24411239-f0d3956c5c614d680d5d68bba&q=yellow+flowers&image_type=photo";
 
-let textapi = 'd57a288445f17aaa352de84954451d29';
+
 
 
 
