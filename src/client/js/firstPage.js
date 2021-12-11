39ad4f91e966b048cea6527134286f100209abb6
Joanna YU <joannayu@Haos-iMac.local>
2021-12-11 21:55:22 +1030

    add countdown days

--- src/client/js/firstPage.js
+++ src/client/js/firstPage.js
@@ -46,7 +46,28 @@ function updateUI (data) {
     document.getElementById('temperature').innerHTML = `<p> Temperature: ${data.temperature} </P>`;
     document.getElementById('wind').innerHTML = `<p> Wind: ${data.wind} </p>`;
     document.getElementById('cloud').innerHTML = `<p> Cloud: ${data.cloud} </p>`;
+
+
+    let countDown = setInterval(function() {
+        let departDate = new Date(document.getElementById('my-depart-date').value).getTime();
+        let now = new Date().getTime();
+
+        let distance = departDate - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+         document.getElementById("countdown").innerHTML = "My trip is " + days + " days away ";
+      
+         if (distance < 0) {
+           clearInterval(countDown);
+         document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
 }
+
+     
+
 
 
 export { generateContent }
